28786a879e28f60bea3cd9e23837cae839611d1b
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-08-17 20:51:45 -0400

    Initial Commit

---
+++ HealthKit Intro Counting Steps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HealthKit Intro Counting Steps
+//
+//  Created by Caroline LaDouce on 8/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HealthKit Intro Counting Steps/HealthKit_Intro_Counting_StepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKit_Intro_Counting_StepsApp.swift
+//  HealthKit Intro Counting Steps
+//
+//  Created by Caroline LaDouce on 8/17/22.
+//
+
+import SwiftUI
+
+@main
+struct HealthKit_Intro_Counting_StepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
